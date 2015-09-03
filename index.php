7cf0068b98b6f2b09516c09fdb69de8d5a5f5b1e
sokeang.lor <sokeang.lor@gmail.com>
2015-09-03 18:31:10 -0500

    hello world

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+    $focus = "FOCUS";
+    $year_present = 2015;
+    $year_born = 1985;
+    $age = $year_present - $year_born;
+    $son_name = "Aiden Sok";
+    $son_age = "3years";
+
+?>
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <title>My PHP World</title>
+    </head>    
+    
+    <body>
+        
+        <h1>Hello World !!</h1>
+        
+        <p>This quarter classes is somewhat hard and need a lot of my full <?php echo $focus ?> and time to study hard.</p> 
+        
+        <p>I will be <?php echo $age ?> this year.</p>
+
+        <p>I have a son name <?php echo $son_name ?> and he is a naughty <?php echo $son_age ?> boy.</p>
+        
+        <p>My most favorite drinks are: Mountain Dew, Mellow Yellow, and Fanta. </p>
+
+        <p><br> <h2>In my free time, i like to </h2>
+            watch ANIME </br>
+            <br> read MANGA books </br>
+            <br> watch Chinese Drama, and </br>
+            <br> spend time with my family. </br>
+        </p>
+    </body>
+</html>
